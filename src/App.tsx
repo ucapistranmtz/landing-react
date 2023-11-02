279864d5b8ebeb1cf8bdc3d5ba8d17a07781eecd
Ulises Capistrán <ucapistranmtz@gmail.com>
2023-11-02 06:22:33 -0500

    not found code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-
+import { NotFoundPage } from "./pages";
 function App() {
   return (
     <>
       <div>
         <h1> hello world</h1>
+        <NotFoundPage />
       </div>
     </>
   );
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <>
+      <h1> 404 error page not found</h1>
+    </>
+  );
+};
