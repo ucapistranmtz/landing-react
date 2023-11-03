2ead759778072dd0056b84e5694e2976ca6c4361
Ulises Capistrán <ucapistranmtz@gmail.com>
2023-11-02 21:05:08 -0500

    not found code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <>
       <div>
-        <h1> hello world</h1>
         <NotFoundPage />
       </div>
     </>
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,25 @@
 export const NotFound = () => {
   return (
     <>
-      <h1> 404 error page not found</h1>
+      <div id="container">
+        <img
+          src="https://res.cloudinary.com/df5flpd2z/image/upload/f_auto,q_auto/ikzrzo7ckfwpeduw0bqx"
+          alt="Rod Serling in the Twilight Zone"
+        />
+        <div className="top-right">
+          <h3>Oops! You've Strayed Into the Twilight Zone</h3>
+        </div>
+
+        <p>
+          Looks like you've wandered into uncharted territory. The page you're
+          looking for has vanished into the digital abyss.
+        </p>
+        <p>
+          While we try to find your lost webpage, feel free to explore some of
+          the otherworldly content we have to offer.
+        </p>
+        <a href="/">Escape Back to Safety!</a>
+      </div>
     </>
   );
 };
